ffdb01940599f82542b1acebb7bf18b092f6a791
fernandobouchet <fernandobouchet@gmail.com>
2024-11-02 13:11:52 -0300

    feat: back: update default port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import { countryRoutes } from "./routes/countryRoute";
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 
 app.use("/api/v1/countries", countryRoutes);
 
